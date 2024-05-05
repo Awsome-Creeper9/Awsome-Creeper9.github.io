a9f37bfbed1bdbabc7df8840d42983fe9c966f55
Awsome-Creeper9 <gherdt@outlook.com>
2024-05-04 22:52:11 -0500

    Updated logo. Hopefully fixed viewport for most landscape-view devices.

--- portfolio/MinecraftClicker/scripts/splashes.js
+++ portfolio/MinecraftClicker/scripts/splashes.js
@@ -1,5 +1,5 @@
 let scaleCooldown = 0;
-let splashes = ["Hello!", "Clicked.", "This is a splash message.", "Will you succeed?", "I am inside of your computer.", "This is a warning!", "Leave!", "Danger!", "*Clicks*", "Attack!", "Minceraft", "\"Java\" + \"Script\" === \"JavaScript\"", "&lthtml&gt&lt/html&gt", "Hello, Json!", "NaN &lt 0 AND 0 &lt NaN", "//true", "false = true", "/execute", "Made with Minecraft commands.", "NaN", "Alt + F4", "rm -rf", "Ctrl + S", "Cookies!"]
+let splashes = ["Hello!", "Clicked.", "This is a splash message.", "Will you succeed?", "I am inside of your computer.", "This is a warning!", "Leave!", "Danger!", "*Clicks*", "Attack!", "Minceraft", "\"Java\" + \"Script\" === \"JavaScript\"", "&lthtml&gt&lt/html&gt", "Hello, Json!", "NaN &lt 0 AND 0 &lt NaN", "//true", "false = true", "/execute", "Made with Minecraft commands.", "NaN", "Alt + F4", "rm -rf", "Ctrl + S", "No cookies (yet)!", "PC Preferred"]
 document.getElementById("splash").innerHTML = splashes[Math.floor(Math.random() * splashes.length)];
 document.getElementById("splash").style.fontSize = (45 - (document.getElementById("splash").innerHTML.length * (3/5))) + "px";
 setTimeout(() => {document.getElementById("splash").style.transition = "1s";}, 20)
