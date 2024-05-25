c11066c9e6b1ce17bdd2f9f0a3df6120889086b5
Awsome-Creeper9 <gherdt@outlook.com>
2024-05-24 23:54:01 -0500

    Changed Nav and slightly tweaked styling. Removed themes/cookies for now

---
+++ scripts/nav-cube.js
@@ -0,0 +1,53 @@
+const blockElement = document.getElementById("block")
+const leftArrow = document.getElementById("leftArrow")
+const rightArrow = document.getElementById("rightArrow")
+let currentRotation = parseInt(blockElement.style.transform.slice(`rotateX(-10deg) rotateY(`.length, blockElement.style.transform.length-1));
+
+rightArrow.addEventListener("mouseenter", () => {
+    if (currentRotation > -180) {
+        blockElement.style.transform = `rotateX(-10deg) rotateY(${currentRotation-20}deg) rotateZ(0deg)`;
+    }
+})
+
+rightArrow.addEventListener("mouseleave", () => {
+    blockElement.style.transform = `rotateX(-10deg) rotateY(${currentRotation}deg) rotateZ(0deg)`;
+})
+
+rightArrow.addEventListener("click", () => {
+    if (currentRotation > -180) {
+        currentRotation -= 90;
+        blockElement.style.transform = `rotateX(-10deg) rotateY(${currentRotation}deg) rotateZ(0deg)`;
+    }
+})
+
+leftArrow.addEventListener("mouseenter", () => {
+    if (currentRotation < 0) {
+        blockElement.style.transform = `rotateX(-10deg) rotateY(${currentRotation+20}deg) rotateZ(0deg)`;
+    }
+})
+
+leftArrow.addEventListener("mouseleave", () => {
+    blockElement.style.transform = `rotateX(-10deg) rotateY(${currentRotation}deg) rotateZ(0deg)`;
+})
+
+leftArrow.addEventListener("click", () => {
+    if (currentRotation < 0) {
+        currentRotation += 90;
+        blockElement.style.transform = `rotateX(-10deg) rotateY(${currentRotation}deg) rotateZ(0deg)`;
+    }
+})
+
+setInterval (() => {
+    if (currentRotation <= -180) {
+        rightArrow.style.color = "#888"
+    }
+    else {
+        rightArrow.style.color = "#fff"
+    }
+    if (currentRotation >= 0) {
+        leftArrow.style.color = "#888"
+    }
+    else {
+        leftArrow.style.color = "#fff"
+    }
+})
