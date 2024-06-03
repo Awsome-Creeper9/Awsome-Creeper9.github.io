bc8623f5f4de3b6d49f215475971b9133574c89a
Awsome-Creeper9 <gherdt@outlook.com>
2024-06-03 16:33:52 -0500

    Attempt Commit 3 of Themes

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,6 +1,9 @@
 let currentTheme;
 setInterval(() => {
-    if (document.cookie.split(";").some((item) => item.includes("theme=dark"))) {
+    if (
+        // document.cookie.split(";").some((item) => item.includes("theme=dark"))
+        cookieStore.get("theme") == "dark"
+    ) {
         if (currentTheme !== "dark") {
             document.getElementById("themesheet").href = "/styles/frontpage-css.css"
             document.getElementById("lightmode").style.display = "unset"
@@ -8,7 +11,10 @@ setInterval(() => {
             currentTheme = "dark"
         }
     }
-    else if (document.cookie.split(";").some((item) => item.includes("theme=light"))) {
+    else if (
+        // document.cookie.split(";").some((item) => item.includes("theme=light"))
+        cookieStore.get("theme") == "light"
+    ) {
         if (currentTheme !== "light") {
             document.getElementById("themesheet").href = "/styles/frontpage-css-light.css"
             document.getElementById("lightmode").style.display = "none"
@@ -17,15 +23,30 @@ setInterval(() => {
         }
     }
     else {
-        document.cookie = "theme=dark; samesite=strict; secure; Domain=awsome-creeper9.github.io; Path=/"
+        // document.cookie = "theme=dark; samesite=strict; secure; Domain=awsome-creeper9.github.io; Path=/"
+        cookieStore.set({
+            name: "theme",
+            value: "dark",
+            domain: "awsome-creeper9.github.io",
+        })
     }
 }, 10)
 
 function setTheme(e) {
     if (e.id == "lightmode") {
-        document.cookie = "theme=light; samesite=strict; secure; Domain=awsome-creeper9.github.io; Path=/"
+        // document.cookie = "theme=light; samesite=strict; secure; Domain=awsome-creeper9.github.io; Path=/"
+        cookieStore.set({
+            name: "theme",
+            value: "light",
+            domain: "awsome-creeper9.github.io",
+        })
     }
     else if (e.id == "darkmode") {
-        document.cookie = "theme=dark; samesite=strict; secure; Domain=awsome-creeper9.github.io; Path=/"
+        // document.cookie = "theme=dark; samesite=strict; secure; Domain=awsome-creeper9.github.io; Path=/"
+        cookieStore.set({
+            name: "theme",
+            value: "dark",
+            domain: "awsome-creeper9.github.io",
+        })
     }
 }
